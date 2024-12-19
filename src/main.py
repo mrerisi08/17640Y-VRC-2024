869c5421ec58cd4c0777860173f744fef04dd831
Max R <mrerisi26@cgps.org>
2024-12-19 15:13:48 -0500

    a

--- src/main.py
+++ src/main.py
@@ -14,32 +14,16 @@ import time
 # Brain should be defined by default
 brain=Brain()
 
-# controllers
 driveController = Controller()
 mechController = Controller()
 
-# drivetrain
 leftMotor = Motor(Ports.PORT11)
 rightMotor = Motor(Ports.PORT14, True)
+
 driveTrain = DriveTrain(leftMotor, rightMotor)
 
-intakeMotor = Motor(Ports.PORT13)
-
-
-
-# listeners
-def driveLeftJoystickChanged():
-    driveTrain.drive(FORWARD, driveController.axis3.position(), PERCENT)
-    # # with open("data.txt", "a") as f:
-    #     # f.write(str(driveController.axis3.position()) + "\n")
-    # brain.screen.clear_screen()
-    # brain.screen.print(driveController.axis3.position())
-    # brain.screen.next_row()
-    # brain.screen.print("ASDJHKL")
-driveController.axis3.changed(driveLeftJoystickChanged)
-# while True:
-#     driveLeftJoystickChanged()
-#     time.sleep(0.15)
+intakeMotor: Motor = Motor(Ports.PORT8)
+intakeMotor.set_velocity(100, PERCENT)
 
 brain.screen.print("Hello V5")
 
@@ -50,3 +34,43 @@ brain.screen.print("Hello V5")
 # motor1.stop()
 
 driveTrain.drive_for(FORWARD, 100)
+
+brain.screen.print(driveController.axis1.position())
+
+def leftjoychange():
+    driveTrain.drive(FORWARD,driveController.axis3.position(),PERCENT)
+def rightjoychange():
+    driveTrain.drive(FORWARD, driveController.axis1.position(), PERCENT)
+
+
+
+def intake_func(forward):
+    if forward:
+        intakeMotor.spin(FORWARD)
+    else:
+        intakeMotor.spin(REVERSE)
+
+
+def stop_intake():
+    intakeMotor.stop()
+
+
+driveController.axis1.changed(rightjoychange)
+driveController.axis3.changed(leftjoychange)
+
+mechController.buttonL1.pressed(intake_func, True)
+mechController.buttonL2.pressed(intake_func, False)
+
+# when L1 or L2 is released, stop_intake()
+mechController.buttonL1.released(stop_intake)
+mechController.buttonL2.released(stop_intake)
+
+pneu = DigitalOut(brain.three_wire_port.b)
+def push_func():
+    pneu.set(True)
+
+def pull_func():
+    pneu.set(False)
+
+mechController.buttonR1.pressed(push_func)
+mechController.buttonR2.pressed(pull_func)
